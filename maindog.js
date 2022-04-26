cb4ea589564c0bd4d56abc6389ec77315f500c42
spacewal <103618601+spacewal@users.noreply.github.com>
2022-04-25 22:11:06 -0400

    Create maindog.js

---
+++ maindog.js
@@ -0,0 +1,21 @@
+// my age 
+var myAge = 30;
+
+//
+var earlyYears = 2;
+
+//
+var earlyYears = earlyYears * 10.5;
+
+//
+var laterYears = myAge - 2;
+
+//
+laterYears = laterYears * 4;
+
+//
+var myAgeInDogYears = earlyYears + laterYears;
+
+myName = 'Alberto' .toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
